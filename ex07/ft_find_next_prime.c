c5a1dbd8336744997ddd26aaa4e8ddbdfa7596bb
Guillaume Baumgartner <gbaumgar@k0r2p13.42mulhouse.fr>
2022-01-30 09:43:46 +0100

    fix ex07

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: gbaumgar <gbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 13:03:56 by gbaumgar          #+#    #+#             */
-/*   Updated: 2022/01/24 12:03:27 by gbaumgar         ###   ########.fr       */
+/*   Updated: 2022/01/24 20:04:09 by gbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@ int	ft_find_next_prime(int nb)
 	int	i;
 	int	r;
 
-	if (nb < 0)
-		return (1);
-	else if (nb < 2)
+	if (nb < 2)
 		return (2);
 	while (1)
 	{
